96f907aca1b29077bb1953b7eb5583298c591d3e
Monika Sikora <monikastanko90@gmail.com>
2020-10-19 16:05:03 +0200

    Navigation and home page added

--- SnS/src/app/app.module.ts
+++ SnS/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { TravelComponent } from './travel/travel.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    TravelComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
