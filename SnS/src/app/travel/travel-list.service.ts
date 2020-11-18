fd34040b81d9ff3dc1b3aaaefd1b3c397f411f89
Monika Sikora <monikastanko90@gmail.com>
2020-11-18 08:55:56 +0100

    Before auth cleanup

--- SnS/src/app/travel/travel-list.service.ts
+++ SnS/src/app/travel/travel-list.service.ts
@@ -1,10 +1,3 @@
-// import { TravelExpand } from './travel-expand/travel-expand.model';
-// import { Injectable } from "@angular/core";
-
-// @Injectable({ providedIn: 'root' })
-// export class TravelListService {
-// private travelExpands: TravelExpand[] = [];
-
   // private travelExpands: TravelExpand[] = [
   //   new TravelExpand (
   //     1,
@@ -94,17 +87,3 @@
   //     '../../../assets/Travel/Expand/Nica/Nica3.jpg'
   //   )
   // ]
-
-//   setTravelList(travelExpands: TravelExpand[]) {
-//     this.travelExpands = travelExpands;
-//   }
-
-//   // to return direct reference to this array - exact copy in case of changing we still have original one, so we really can access it from outside
-//   getTravelList() {
-//     return this.travelExpands.slice();
-//   }
-
-//   getTravelCountry(index: number){
-//     return this.travelExpands[index];
-//   }
-// }
